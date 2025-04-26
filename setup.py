6eac74ab6d2966a7034e0290adeed31855e345a8
omarDeveloper <ogiovannigomez@ucundinamarca.edu.co>
2025-04-25 23:59:17 -0500

    cambios generados

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="OmarGomezDeveloper",                         # Tu nombre
+    author_email="ogiovannigomez@ucundinamarca.edu.co",                 # Tu correo electrónico
+    url="https://github.com/OmarGomezDeveloper/TercerRepo.git",     # URL del proyecto
+)
